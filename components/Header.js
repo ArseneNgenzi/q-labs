591153f5e00e7ca67cf1bd9b3a9c81cd6cba507d
ArseneNgenzi <arsenengenzi@gmail.com>
2024-02-14 20:00:11 +0200

    bug fix

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 import Socials from "../components/Socials";
@@ -7,7 +6,7 @@ const Header = () => {
 	return (
 		<header className=" lg:pt-8 xl:pt-0  w-full flex items-center lg:px-16 xl:px-0 z-50">
 			<div className=" container mx-auto z-10">
-        <div className="lg:pt-8 flex flex-row justify-between items-center">
+        <div className=" pt-4 lg:pt-8 flex flex-row justify-between items-center">
           {/* Logo */}
           <Link href={'/'}> <p className="h2 text-white">Q-<span className=" text-accent">Labs</span> </p></Link>
           {/* Socials */}
